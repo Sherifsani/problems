7d879c1585dfaa937cf06eea5b90789a9187cccb
Sherif Sani <sanisherif11@gmail.com>
2025-11-25 12:44:04 +0100

    implement minOperationsBrute method to calculate minimum operations based on box weights

---
+++ minimum-number-operations/min-operation.py
@@ -0,0 +1,14 @@
+from typing import List
+
+class Solution:
+    
+    # brute force approach
+    def minOperationsBrute(self, boxes: str) -> int:
+        n = len(boxes)
+
+        answer = [0] * n
+        for i in range(n):
+            for j in range(n):
+                if boxes[j] != '0':
+                    answer[i] += abs(i - j) * int(boxes[j])
+        return answer
