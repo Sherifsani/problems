42ad4aec31538b866060307036fb4f73614640d4
Sherif Sani <sanisherif11@gmail.com>
2025-11-26 23:46:09 +0100

    .

--- minimum-number-operations/min-operation.py
+++ minimum-number-operations/min-operation.py
@@ -3,7 +3,7 @@ from typing import List
 class Solution:
 
     # brute force approach
-    def minOperationsBrute(self, boxes: str) -> List[int]:
+    def minOperationsI(self, boxes: str) -> List[int]:
         n = len(boxes)
 
         answer = [0] * n
@@ -12,3 +12,29 @@ class Solution:
                 if boxes[j] != '0':
                     answer[i] += abs(i - j) * int(boxes[j])
         return answer
+    
+    # optimal solution
+    def minOperationsII(self, boxes: str) -> List[int]:
+        n = len(boxes)
+        answer = [0] * n
+
+        specific_ops = ball_count = 0
+
+        for i in range(n):
+            answer[i] +=  1
+            if boxes[i] == '1':
+                ball_count += 1
+            
+            specific_ops += ball_count
+        
+        specific_ops = ball_count = 0
+
+        for i in range(n-1, 1, -1):
+            answer[i] += specific_ops
+            if boxes[i] == 1:
+                ball_count += 1
+            
+            specific_ops += ball_count
+        
+        return answer
+
