1bd03260d37899e68112c3ccae5e6693ae5e1679
Sherif Sani <sanisherif11@gmail.com>
2025-12-02 08:57:36 +0100

    remove unused variable in findDisappearedNumbers method for cleaner code

--- numbers-dissappeared-in-array/dissappeared.py
+++ numbers-dissappeared-in-array/dissappeared.py
@@ -4,7 +4,6 @@ class Solution:
     # solution 1 (mapping)
     def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
         num_dict = {}
-        res = []
         for i in range(1, len(nums) + 1):
             num_dict[i] = 0
         for i in nums:
