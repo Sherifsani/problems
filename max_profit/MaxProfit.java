ff0553e861822a382bf569f4f00bae8d4f5d2c9a
Sherif Sani <sanisherif11@gmail.com>
2025-12-05 20:38:50 +0100

    implement MaxProfit class to calculate maximum profit from stock prices

---
+++ max_profit/MaxProfit.java
@@ -0,0 +1,21 @@
+public class MaxProfit {
+    public static void main(String[] args) {
+        int[] prices = {7,6,4,3,1};
+        System.out.println("Maximum Profit: " + getMaxProfit(prices));
+    }
+    
+    public static int getMaxProfit(int[] prices) {
+        int minPrice = prices[0];
+        int max_profit = 0;
+
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] <= minPrice) {
+                minPrice = prices[i];
+                continue;
+            }
+            int profit = prices[i] - minPrice;
+            max_profit = Math.max(max_profit, profit);
+        }
+        return max_profit;
+    }
+}
